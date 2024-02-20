912b88eb1db0e23685c2cf929f9d31741f66e691
Wagner Almeida <wagneralmeida@Wagners-MacBook-Pro.local>
2024-02-19 23:34:43 -0300

    obervability

--- src/main/java/br/com/itau/geradornotafiscal/web/controller/GeradorNFController.java
+++ src/main/java/br/com/itau/geradornotafiscal/web/controller/GeradorNFController.java
@@ -1,11 +1,11 @@
 package br.com.itau.geradornotafiscal.web.controller;
 
 import br.com.itau.geradornotafiscal.model.NotaFiscal;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import br.com.itau.geradornotafiscal.model.Pedido;
 import br.com.itau.geradornotafiscal.service.GeradorNotaFiscalService;
+import io.micrometer.core.annotation.Timed;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/pedido")
 @RequiredArgsConstructor
+@Slf4j
 public class GeradorNFController {
 
 	private final GeradorNotaFiscalService notaFiscalService;
 
 	@PostMapping("/gerarNotaFiscal")
+	@Timed
 	public ResponseEntity<NotaFiscal> gerarNotaFiscal(@RequestBody Pedido pedido) {
 		// Lógica de processamento do pedido
+
 		// Aqui você pode realizar as operações desejadas com o objeto Pedido
 
 		// Exemplo de retorno
@@ -30,5 +33,6 @@ public class GeradorNFController {
 		NotaFiscal notaFiscal = notaFiscalService.gerarNotaFiscal(pedido);
 		return new ResponseEntity<>(notaFiscal, HttpStatus.OK);
 	}
+
 	
 }
